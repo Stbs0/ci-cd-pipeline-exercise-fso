22436a16e8d0651f0e8d230aff579616726ac245
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-09-01 13:09:30 +0300

    cccccc

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 let PORT = process.env.PORT;
 
-const MONGODB_URI = process.env.TEST_MONGODB_URI;
+const MONGODB_URI = process.env.NODE_ENV === 'production'  ? process.env.MONGODB_URI : process.env.TEST_MONGODB_URI;
 
 module.exports = {
   MONGODB_URI,
